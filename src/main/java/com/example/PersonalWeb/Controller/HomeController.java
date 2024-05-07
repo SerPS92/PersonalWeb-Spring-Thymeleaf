cee1047aa6025664307ab91894b678b4ac918653
SerPS92 <sergio.rianxo92@gmail.com>
2024-05-07 15:47:22 +0200

    Error page & responsive

--- src/main/java/com/example/PersonalWeb/Controller/HomeController.java
+++ src/main/java/com/example/PersonalWeb/Controller/HomeController.java
@@ -3,9 +3,11 @@ package com.example.PersonalWeb.Controller;
 import com.example.PersonalWeb.service.MailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Controller
 @RequestMapping("")
@@ -28,7 +30,7 @@ public class HomeController {
                        @RequestParam(name = "from") String from,
                        @RequestParam(name = "text") String text){
 
-        String to = "";
+        String to = "sergio.rianxo92@gmail.com";
         String subject = "Mensaje recibido desde el blog";
         String content = "Name: " + name + "\n" + "Email: " + from + "\n" + "\n" + text;
         mailSenderService.sendEmail(to, subject, content);
@@ -40,4 +42,5 @@ public class HomeController {
     public String thanks(){
         return "thanks";
     }
+
 }
